386b497d00cff2744d7cca1559efdc6d71b8542e
cprtmchzh <bin54511563@gmail.com>
2025-02-18 17:46:50 +0900

    기숙사 공지 크롤링 추가

--- Back_End/board_list.py
+++ Back_End/board_list.py
@@ -8,7 +8,7 @@ app.config['JSON_AS_ASCII'] = False
 CORS(app)
 
 @app.route("/notice", methods = ["GET"])
-def index():
+def notice():
     url = request.args.get("url")
     req = urllib.request.Request(url)
     sourcecode = urllib.request.urlopen(url).read()
@@ -38,6 +38,31 @@ def index():
         
     return jsonify(notice_list)
     
+    
+@app.route("/dormitory", methods = ["GET"])
+def dormitory():
+    url = request.args.get("url")
+    req = urllib.request.Request(url)
+    sourcecode = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(sourcecode, "html.parser")
+    notice_list = []
+    
+    board_list = soup.select("#bd_163_0 > div > table > tbody > tr")
+    
+    for board in board_list:
+        type = board.find(class_="no").text.strip()
+        name = board.find(class_="title").text.strip()
+        writer = board.find(class_="author").text.strip()
+        date = board.find(class_="time").text.strip()
+        
+        #bd_163_0 > div.bd_lst_wrp > table > tbody > tr:nth-child(10) > td.title > a
+        
+        hyperlink = board.select_one("td.title > a").attrs['href']
+        
+        new_notice = {"type": type, "name": name, "writer": writer, "date": date, "hyperlink": hyperlink}
+        notice_list.append(new_notice)
+        
+    return jsonify(notice_list)
 
 if __name__ == '__main__':
     app.run(port=5000, debug=True)
