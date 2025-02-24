8892b1cd09d15e5edf288ac058073149b6e7763f
cprtmchzh <bin54511563@gmail.com>
2025-02-24 15:23:59 +0900

    컴포넌트 fixed 해제

--- front_end/src/app/layout.tsx
+++ front_end/src/app/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import Header from '../components/header';
+import '../ui/globals.css';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
@@ -9,10 +9,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <title>Document</title>
             </head>
-            <body className="bg-background">
-                <Header></Header>
-                {children}
-            </body>
+            <body className="p-4 mx-auto max-w-screen-2xl md:p-6">{children}</body>
         </html>
     );
 }
--- front_end/src/app/notice/layout.tsx
+++ front_end/src/app/notice/layout.tsx
@@ -4,14 +4,16 @@ import Sidebar from '../../components/sidebar';
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (
         <>
-            <Sidebar />
-            <div className="absolute top-12 left-1/4 bottom-9 shadow-sm p-[20px] w-[1050px] h-[700x] rounded-3xl overflow-x-auto bg-white">
-                <table className="w-full border-collapse mt-2.5">
-                    <thead>
-                        <tr className="p-3 text-center font-bold text-gray-400 border-b-[1px]"></tr>
-                    </thead>
-                    {children}
-                </table>
+            <div className="grid grid-cols-1 gap-4 sm:grid-cols-4 md:gap-6">
+                <Sidebar />
+                <div className="rounded-2xl border p-5 md:p-6 bg-white col-span-3">
+                    <table className="w-full mt-2.5">
+                        <thead>
+                            <tr className="p-3 text-center font-bold text-gray-400 border-b-[1px]"></tr>
+                        </thead>
+                        {children}
+                    </table>
+                </div>
             </div>
         </>
     );
